6504d90cd98b0b5ebdee8accc91e09d09e0c88af
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2023-11-08 15:20:28 +0530

    chages

--- src/pages/10.tsx
+++ src/pages/10.tsx
@@ -19,7 +19,7 @@ TagManager.initialize(tagManagerArgs);
 
 export default function Fifth_SP() {
   useEffect(() => {
-    window.document.title = "Senior's Allowance  Program 2023";
+    window.document.title = "Senior's Flex  Program 2023";
 
     axios
       .get(process.env.REACT_APP_PROXY + `/visits/8`)
@@ -224,7 +224,7 @@ export default function Fifth_SP() {
   return (
     <div>
       <div className="top-sticky-blue-test2" id="top">
-      Senior's Allowance  Program 2023
+      Senior's Flex  Program 2023
       </div>
       {step === "process" ? (
         <>
@@ -232,22 +232,22 @@ export default function Fifth_SP() {
             <div className="main-descrition-5">
               <div className="main-des-title-6">
                 <b>
-                  Americans Over 60 Can Now Qualify For The $3600 Allowance  Card In
+                  Americans Over 60 Can Now Qualify For The $3600 Flex  Card In
                   2023. Here's How!
                 </b>
               </div>
               {/* <img className='topic-img-larger' src = {Headline} alt = "head"/> */}
               <img className="topic-img-middle" src={Head_bg} alt="head" />
               <div className="main-des-5">
-                Americans over 60 years old can claim the 2023 Allowance  Spending
+                Americans over 60 years old can claim the 2023 Flex  Spending
                 Card that gives them up to $3600. Americans can use the funds to
                 fully cover the cost of their monthly expenses such as
                 Groceries, Rent, Bills and any other expenses they may have!
               </div>
               <div className="main-des-5" style={{ marginTop: "1rem" }}>
-                If you have not yet claimed your monthly allowance then answer
+                If you have not yet claimed your monthly Flex then answer
                 the questions below and once approved <b>you will have your $3,600
-                Allowance  Card mailed to you within a few days ready for use!</b>
+                Flex  Card mailed to you within a few days ready for use!</b>
               </div>
               {/* <div className='main-des-5' style = {{marginTop:"1rem"}}><b>Simplemente responda las siguientes preguntas:</b></div> */}
             </div>
@@ -274,7 +274,7 @@ export default function Fifth_SP() {
         <div className="checking">
           <div className="congrats">Congratulation, You Qualify!</div>
           <div className="top-description-5">
-            Make A <b>Quick Call</b> To Claim Your Allowance  Card!
+            Make A <b>Quick Call</b> To Claim Your Flex  Card!
           </div>
           <div className="spots-count">Spots remaining: 4</div>
           <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
