a7ed2b5926e8870f2cdf9b45827c824e54a5d6c4
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-03-15 19:08:10 +0530

    Update Newx.tsx

--- src/pages/Newx.tsx
+++ src/pages/Newx.tsx
@@ -282,9 +282,9 @@ export default function Fifth_SP() {
           </div>
           <div className="spots-count">Spots remaining: 4</div>
           <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
-          <a href="tel:+18446720874">
+          <a href="tel:+18775370627">
             <div className="call-btn" onClick={handleCall}>
-            CALL (844) 672-0874
+CALL (877) 537-0627
             </div>
           </a>
           <div className="sub-title">We Have Reserved Your Spot</div>
